426fa6d529b5597a5d589529f3d71b0122f1da09
ubuntu <jesus.abarca.rodriguez@est.una.ac.cr>
2024-03-22 23:25:35 -0600

    UPDATE main.cpp in First Exercise

--- 1-fahrenheit-celsius/src/main.cpp
+++ 1-fahrenheit-celsius/src/main.cpp
@@ -2,27 +2,13 @@
 #include <iomanip>
 using namespace std;
 int main() {
-    // Variables para almacenar los límites y el incremento de la tabla
-    int inicio, fin, incremento;
 
-    // Solicitar al usuario que ingrese los límites y el incremento de la tabla
-    cout << "Ingrese el inicio de la tabla (en grados Fahrenheit): ";
-    cin >> inicio;
-    cout << "Ingrese el fin de la tabla (en grados Fahrenheit): ";
-    cin >> fin;
-    cout << "Ingrese el incremento de la tabla (en grados Fahrenheit): ";
-    cin >> incremento;
-
-    // Imprimir encabezado de la tabla
     cout << "Fahrenheit\tCelsius" << endl;
     cout << "-----------------------" << endl;
 
-    // Calcular y mostrar la tabla de conversiones
-    for (int fahrenheit = inicio; fahrenheit <= fin; fahrenheit += incremento) {
-        // Calcular la temperatura en Celsius
+    for (int fahrenheit = 0; fahrenheit <= 300; fahrenheit += 20) {
         double celsius = (5.0 / 9.0) * (fahrenheit - 32);
 
-        // Imprimir la fila de la tabla con tabulaciones
         cout << fixed << setw(10) << setprecision(2) << fahrenheit << "\t"
              << setw(7) << setprecision(2) << celsius << endl;
     }
